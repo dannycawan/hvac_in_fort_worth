2169289f72a0dd7373a5803ce603aa25cdb73945
dannycawan <setyaawan3@gmail.com>
2025-09-10 14:20:07 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,22 @@
-pluginManagement {
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+buildscript {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
-rootProject.name = "hvac_in_fortworth"
-include(":app")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
