3ef51b8d30d4d76eed74d51ee332a40395948acc
dannycawan <setyaawan3@gmail.com>
2025-09-10 14:21:30 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.hvac.fortworth"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.hvac.fortworth"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -18,57 +24,17 @@ android {
         jvmTarget = "11"
     }
 
-    defaultConfig {
-        applicationId = "com.hvac.fortworth"
-        minSdk = 19
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-        vectorDrawables.useSupportLibrary = true
-    }
-
-    signingConfigs {
-        create("release") {
-            storeFile = file("../keystore/your_release_key.jks") // ganti path sesuai lokasi keystore
-            storePassword = "your_store_password" // ganti sesuai password keystore
-            keyAlias = "your_key_alias"           // ganti sesuai alias key
-            keyPassword = "your_key_password"     // ganti sesuai password key
-        }
-    }
-
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true       // aktifkan Proguard
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
-        debug {
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
     }
-
-    packaging {
-        resources {
-            excludes += setOf(
-                "META-INF/LICENSE*",
-                "META-INF/NOTICE*"
-            )
-        }
-    }
-}
-
-flutter {
-    source = "../.."
 }
 
 dependencies {
-    // AdMob SDK terbaru
-    implementation("com.google.android.gms:play-services-ads:23.5.0")
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
 }
