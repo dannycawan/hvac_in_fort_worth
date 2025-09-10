ccac215f1f2577e2ae213067a87f199d1d90a2ce
dannycawan <setyaawan3@gmail.com>
2025-09-10 13:52:59 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,18 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // Flutter Gradle Plugin harus terakhir
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.hvac.fortworth"
     compileSdk = 35
-    ndkVersion = "27.0.12077973" // sesuai kebutuhan dependency
 
     defaultConfig {
         applicationId = "com.hvac.fortworth"
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
-        vectorDrawables.useSupportLibrary = true
+        versionName = "1.0"
     }
 
     compileOptions {
@@ -27,38 +23,4 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug") // ganti dengan release signing sebelum upload
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-        }
-    }
-
-    packaging {
-        resources {
-            excludes += setOf(
-                "META-INF/LICENSE*",
-                "META-INF/NOTICE*"
-            )
-        }
-    }
-}
-
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    // AdMob SDK
-    implementation("com.google.android.gms:play-services-ads:23.1.0")
 }
