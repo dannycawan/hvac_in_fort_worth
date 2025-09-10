ab2cac7c7c737c9de8ef4cdd114d60b1a70840bc
dannycawan <setyaawan3@gmail.com>
2025-09-10 14:18:48 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,26 +1,10 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.hvac.fortworth"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
+
+rootProject.name = "hvac_in_fortworth"
+include(":app")
