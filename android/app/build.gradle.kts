56bb970e9c7fbc2d75236ddc16b0dd9828207387
dannycawan <setyaawan3@gmail.com>
2025-09-10 12:45:05 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // Flutter Gradle Plugin harus terakhir
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.hvac.fortworth"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // pakai versi terbaru sesuai kebutuhan dependency
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,31 +20,37 @@ android {
 
     defaultConfig {
         applicationId = "com.hvac.fortworth"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-
-        // opsional: agar tidak crash karena WebView di Android 11+
+        minSdk = 19
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("../keystore/your_release_key.jks") // ganti path sesuai lokasi keystore
+            storePassword = "your_store_password" // ganti sesuai password keystore
+            keyAlias = "your_key_alias"           // ganti sesuai alias key
+            keyPassword = "your_key_password"     // ganti sesuai password key
+        }
     }
 
     buildTypes {
         release {
-            // TODO: ganti dengan signingConfig release sebelum upload ke Play Store
-            signingConfig = signingConfigs.getByName("debug")
-            // sementara biar build cepat & stabil
-            isMinifyEnabled = false
-            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true       // aktifkan Proguard
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         debug {
-            // opsional, supaya debug lebih gampang
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
@@ -64,6 +69,6 @@ flutter {
 }
 
 dependencies {
-    // AdMob SDK
-    implementation("com.google.android.gms:play-services-ads:23.1.0")
+    // AdMob SDK terbaru
+    implementation("com.google.android.gms:play-services-ads:23.5.0")
 }
