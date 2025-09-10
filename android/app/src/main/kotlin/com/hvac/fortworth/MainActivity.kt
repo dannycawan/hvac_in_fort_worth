2ab5b5ee57909295a823baca6f062c6143de13d5
dannycawan <setyaawan3@gmail.com>
2025-09-10 12:42:19 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/hvac/fortworth/MainActivity.kt
+++ android/app/src/main/kotlin/com/hvac/fortworth/MainActivity.kt
@@ -2,4 +2,7 @@ package com.hvac.fortworth
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    // Tidak perlu menambahkan kode lain
+    // Semua logika Ads (Banner, Interstitial, Native, App Open) sudah di-handle di Dart (main.dart)
+}
